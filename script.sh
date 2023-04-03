b8ffeb8fe44db979a0779183df4424200257d7fa
betrider <tlsfpem@gmail.com>
2023-04-03 11:10:23 +0900

    merge

--- script.sh
+++ script.sh
@@ -1,13 +1,19 @@
 echo "Welcome to retrofit utilities:"
 echo
-echo "[1] build runner"
+echo "[1] dart build runner"
+echo "[1] flutter build runner"
 echo
 read -p "Run: " selection
 
 case $selection in
 
     1)
-    echo "gql generate"
+    echo "dart .g generate"
+    dart pub run build_runner build
+    ;;
+
+    2)
+    echo "flutter .g generate"
     flutter pub run build_runner build
     ;;
 
